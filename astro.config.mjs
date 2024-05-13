d5edee37f1dcf424a9ac3c45387671337de87f99
terisuke <terisuke1115@gmail.com>
2024-05-14 00:00:54 +0900

    addGSC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     optimizeHoistedScript: true,
   },
-  integrations: [tailwind(), compress()],
+  site: 'https://cor-jp.com',
+  integrations: [tailwind(), compress(), sitemap()],
 });
