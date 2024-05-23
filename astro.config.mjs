91f3d7a278a44f393875796bfdd6d244c2d58302
terisuke <terisuke1115@gmail.com>
2024-05-23 17:26:12 +0900

    couldnotresolvealpine

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,26 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    optimizeHoistedScript: true,
+  },
   site: 'https://cor-jp.com',
   integrations: [tailwind(), compress(), sitemap(),],
+  // Viteのカスタム設定を追加
+  vite: {
+    // Viteの設定をここに記述
+    resolve: {
+      // 特定のモジュールへのパスエイリアスや依存関係の解決設定
+    },
+    optimizeDeps: {
+      // 依存関係の事前バンドルに関する設定
+      include: ['alpinejs', '@alpinejs/collapse']
+    },
+    build: {
+      // ビルドプロセスに関する追加の設定
+    },
+    plugins: [
+      // 必要に応じてViteプラグインを追加
+    ]
+  },
 });
